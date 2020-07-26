b054ed89167861da8627a0a9aba7f9bd066b259f
Tomachi (ICHIGO) <lego@aol.jp>
2020-07-26 13:51:21 +0900

    add: イベント対象期間外の処理追加

--- src/main/java/com/jaoafa/jaoSurvival/Event/Event_JoinVerifiedCheck.java
+++ src/main/java/com/jaoafa/jaoSurvival/Event/Event_JoinVerifiedCheck.java
@@ -1,6 +1,7 @@
 package com.jaoafa.jaoSurvival.Event;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.UUID;
 
 import org.bukkit.ChatColor;
@@ -38,6 +39,26 @@ public class Event_JoinVerifiedCheck implements Listener {
 			return;
 		}
 		String permission = data.getString("permission");
+
+		Date start = new Date(1596240000 * 1000); // 2020/08/01 00:00:00 JST
+		Date now = new Date();
+		Date end = new Date(1598918399 * 1000); // 2020/08/31 23:59:59 JST
+		if (now.before(start) &&
+				!permission.equalsIgnoreCase("Admin") &&
+				!permission.equalsIgnoreCase("Moderator")) {
+			// 期間前 & AM以外
+			event.disallow(Result.KICK_OTHER, ChatColor.GREEN + "[2020SurvivalEvent]\n"
+					+ ChatColor.RESET + "イベントはまだ開始されていません。イベント開始までしばらくお待ちください。 (" + permission + ")");
+			return;
+		}
+		if (now.after(end) &&
+				!permission.equalsIgnoreCase("Admin") &&
+				!permission.equalsIgnoreCase("Moderator")) {
+			// 期間後 & AM以外
+			event.disallow(Result.KICK_OTHER, ChatColor.GREEN + "[2020SurvivalEvent]\n"
+					+ ChatColor.RESET + "イベントは終了いたしました。ご参加ありがとうございました。 (" + permission + ")");
+			return;
+		}
 
 		if (!permission.equalsIgnoreCase("Admin") &&
 				!permission.equalsIgnoreCase("Moderator") &&
